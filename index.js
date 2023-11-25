696e8f4909bdfc61fd28a3dd33222efc744602ec
Muhammad Saikat <smsaikat000@gmail.com>
2023-11-25 15:49:20 +0600

    added jwt

--- index.js
+++ index.js
@@ -1,6 +1,52 @@
-const express=require('express')
-const app=express()
-const port =process.env.PORT || 8000 
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config()
+const jwt = require('jsonwebtoken');
+const port = process.env.PORT || 5000
+app.use(express.json())
+app.use(cors())
+
+const { MongoClient, ServerApiVersion, Code } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@cluster0.aezjkqe.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    const Apartments = client.db('Building').collection('Apartments')
+
+    app.get('/apartments',async(req,res)=>{
+        const result=await Apartments.find().toArray()
+        res.send(result)
+    })
+
+    //! implement jwt
+    app.post('/jwt', (req, res) => {
+    const user = req.body;
+    const token = jwt.sign(user, process.env.JWT, { expiresIn: "1d" });
+    res.send({ token });
+});
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.get('/',(req,res)=>{
     res.send('WelCome')
 })
