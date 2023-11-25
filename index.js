e5487b468b767822a62cbc81d0f87b207264743a
Muhammad Saikat <smsaikat000@gmail.com>
2023-11-25 16:36:19 +0600

    added user

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-require('dotenv').config()
-const jwt = require('jsonwebtoken');
-const port = process.env.PORT || 5000
-app.use(express.json())
-app.use(cors())
+const cors = require("cors");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const port = process.env.PORT || 5000;
+app.use(express.json());
+app.use(cors());
 
-const { MongoClient, ServerApiVersion, Code } = require('mongodb');
+const { MongoClient, ServerApiVersion, Code } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@cluster0.aezjkqe.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -16,7 +16,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -24,22 +24,37 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
-    const Apartments = client.db('Building').collection('Apartments')
+    const Apartments = client.db("Building").collection("Apartments");
+    const Users = client.db("Building").collection("Users");
 
-    app.get('/apartments',async(req,res)=>{
-        const result=await Apartments.find().toArray()
-        res.send(result)
-    })
+    app.post("/addUser", async (req, res) => {
+      const user = req.body;
+     
+      const query={email:user.email}
+      const userExits=await Users.findOne(query)
+      if(userExits){
+        return 
+      }
+      const result = await Users.insertOne(user);
+      res.send(result);
+    });
+    // ? get all apartments
+    app.get("/apartments", async (req, res) => {
+      const result = await Apartments.find().toArray();
+      res.send(result);
+    });
 
     //! implement jwt
-    app.post('/jwt', (req, res) => {
-    const user = req.body;
-    const token = jwt.sign(user, process.env.JWT, { expiresIn: "1d" });
-    res.send({ token });
-});
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.JWT, { expiresIn: "1d" });
+      res.send({ token });
+    });
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -47,7 +62,7 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.get('/',(req,res)=>{
-    res.send('WelCome')
-})
-app.listen(port)
+app.get("/", (req, res) => {
+  res.send("WelCome");
+});
+app.listen(port);
