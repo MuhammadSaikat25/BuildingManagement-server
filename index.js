916cf63b7e75f9ae19033585133dbe075b2bfc05
Muhammad Saikat <smsaikat000@gmail.com>
2023-11-30 09:33:48 +0600

    added payment

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
+const stripe = require("stripe")(process.env.Payment_Key);
+// console.log(process.env.Payment_Key)
 
 const VerifyJwt = (req, res, next) => {
   const authorization = req.headers.authorization;
@@ -48,6 +50,7 @@ async function run() {
     const Users = client.db("Building").collection("Users");
     const Agreement = client.db("Building").collection("Agreement");
     const Coupon = client.db("Building").collection("Coupon");
+    const Payment = client.db("Building").collection("Payment");
     // ! add user
     app.post("/addUser", async (req, res) => {
       const user = req.body;
@@ -81,42 +84,45 @@ async function run() {
       const result = await Apartments.find().toArray();
       res.send(result);
     });
-    // ! get all members 
-    app.get('/allMembers',VerifyJwt,VerifyAdmin,async(req,res)=>{
-      const query={role:'member'}
-      const result=await Agreement.find(query).toArray()
-      res.send(result)
-    })
+    // ! get all members
+    app.get("/allMembers", VerifyJwt, VerifyAdmin, async (req, res) => {
+      const query = { role: "member" };
+      const result = await Agreement.find(query).toArray();
+      res.send(result);
+    });
     // ! get all Agreement request
-    app.get('/getAgreement',VerifyJwt,VerifyAdmin,async(req,res)=>{
+    app.get("/getAgreement", VerifyJwt, VerifyAdmin, async (req, res) => {
       const query = {
-        $and: [
-          { role:'user' },
-          { status: 'pending' }
-        ]
-      }
-      const result=await Agreement.find(query).toArray()
-      res.send(result)
-    })
+        $and: [{ role: "user" }, { status: "pending" }],
+      };
+      const result = await Agreement.find(query).toArray();
+      res.send(result);
+    });
+    // ! my apartment (member) for payment
+    app.get("/myBooking/:email", VerifyJwt, async (req, res) => {
+      const email = req.params.email;
+      const query = {
+        $and: [{ user: email }, { role: "member" }, { status: "checked" }],
+      };
+      const result = await Agreement.findOne(query);
+      res.send(result);
+    });
     // ! get user role
     app.get("/userRole/:email", VerifyJwt, async (req, res) => {
       const user = req.params.email;
       const query = { email: user };
-      const result = await Users.findOne(query); 
+      const result = await Users.findOne(query);
       res.send(result);
     });
-    // ! get login users agreement request data 
-    app.get('/userAgreement/:email',VerifyJwt,async(req,res)=>{
-      const email=req.params.email 
+    // ! get login users agreement request data
+    app.get("/userAgreement/:email", VerifyJwt, async (req, res) => {
+      const email = req.params.email;
       const query = {
-        $and: [
-          { user:email },
-          { status: 'pending' }
-        ]
-      }
-      const result=await Agreement.find(query).toArray()
-      res.send(result)
-    })
+        $and: [{ user: email }, { status: "pending" }],
+      };
+      const result = await Agreement.find(query).toArray();
+      res.send(result);
+    });
     //! implement jwt
     app.post("/jwt", (req, res) => {
       const user = req.body;
@@ -129,58 +135,89 @@ async function run() {
       const result = await Coupon.insertOne(data);
       res.send(result);
     });
-    // ! handel user agreements request 
-    app.patch('/handelAgreement/:id',VerifyJwt,VerifyAdmin, async(req,res)=>{
-      const id=req.params.id
-      const data=req.body
-      const query={_id :new ObjectId(id)}
-      const updateDoc={
-        $set:{
-         ...data
-        }
+    // ! handel user agreements request
+    app.patch(
+      "/handelAgreement/:id",
+      VerifyJwt,
+      VerifyAdmin,
+      async (req, res) => {
+        const id = req.params.id;
+        const data = req.body;
+        const query = { _id: new ObjectId(id) };
+        const updateDoc = {
+          $set: {
+            ...data,
+          },
+        };
+        const result = await Agreement.updateOne(query, updateDoc);
+        res.send(result);
       }
-      const result=await Agreement.updateOne(query,updateDoc)
-      res.send(result)
-    })
-    // ! remove member and chance role into user 
-    app.patch('/removeMember/:id',VerifyJwt,VerifyAdmin,async(req,res)=>{
-      const id=req.params.id
-      const data=req.body
-      const query={_id:new ObjectId(id)} 
-      const updateDoc={
-        $set:{
-         ...data
-        }
-      }
-      const result=await Agreement.updateOne(query,updateDoc)
-      res.send(result)
-    })
-    // ! user into member when agreements request is accept 
-    app.patch('/userToMember/:email',VerifyJwt,VerifyAdmin,async(req,res)=>{
-      const email=req.params.email
-      const data=res.body 
-      const query ={email:email}
-      const updateDoc={
-        $set:{
+    );
+    // ! remove member and chance role into user
+    app.patch("/removeMember/:id", VerifyJwt, VerifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      const query = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
           ...data,
-          role:"member"
-        }
+        },
+      };
+      const result = await Agreement.updateOne(query, updateDoc);
+      res.send(result);
+    });
+    // ! user into member when agreements request is accept
+    app.patch(
+      "/userToMember/:email",
+      VerifyJwt,
+      VerifyAdmin,
+      async (req, res) => {
+        const email = req.params.email;
+        const data = res.body;
+        const query = { email: email };
+        const updateDoc = {
+          $set: {
+            ...data,
+            role: "member",
+          },
+        };
+        const result = await Users.updateOne(query, updateDoc);
+        res.send(result);
       }
-      const result=await Users.updateOne(query,updateDoc)
-      res.send(result)
-    })
+    );
     // ! member into user when agreements request is reject
-    app.patch('/memberToUser/:email',VerifyJwt,VerifyAdmin,async(req,res)=>{
-      const email=req.params.email
-      const data=res.body 
-      const query ={email:email}
-      const updateDoc={
-        $set:{
-          ...data,
-          role:"user"
-        }
+    app.patch(
+      "/memberToUser/:email",
+      VerifyJwt,
+      VerifyAdmin,
+      async (req, res) => {
+        const email = req.params.email;
+        const data = res.body;
+        const query = { email: email };
+        const updateDoc = {
+          $set: {
+            ...data,
+            role: "user",
+          },
+        };
+        const result = await Users.updateOne(query, updateDoc);
+        res.send(result);
       }
-      const result=await Users.updateOne(query,updateDoc)
+    );
+    // ! payment related api
+    app.post("/create-payment-intent", async (req, res) => {
+      const { rent } = req.body;
+      const amount = parseInt(rent * 100);
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.send({ clientSecret: paymentIntent.client_secret });
+    });
+    app.post('/receivePayment',VerifyJwt,async(req,res)=>{
+      const data=req.body 
+      const result=await Payment.insertOne(data)
       res.send(result)
     })
     //! ----------------------------------------------
